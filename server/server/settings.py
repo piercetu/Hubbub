877ee02dda2f6101e570bbe557951d250b81d36c
Nhat Trinh <goodyshoes_nt@yahoo.com>
2019-02-09 17:46:45 -0800

    finished setting up django

--- server/server/settings.py
+++ server/server/settings.py
@@ -78,16 +78,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASS'),
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+DATABASES = {}
 
 #Using Argon2 as the main password hasher
 
